c8dd65de4f638b963ac58c1046a9827d82f756e9
HeadlessChild <markusslindberg7@hotmail.com>
2014-10-10 16:09:16 +0200

    Added Eraser

--- paint.py
+++ paint.py
@@ -11,66 +11,70 @@ linesize = 2
 
 def main():
     root = Tk()
-    drawing_area = Canvas(root, width=800, height=600)
+    drawing_area = Canvas(root, width=1280, height=720, background="white")
     drawing_area.pack()
     drawing_area.bind("<Motion>", motion)
     drawing_area.bind("<ButtonPress-1>", b1down)
     drawing_area.bind("<ButtonRelease-1>", b1up)
 
-    button1 = Button(root, text = "Clear", command = restart_program, anchor = N)
-    button1.configure(width = 3, background = "#FFFFFF", relief = FLAT)
-    button1_window = drawing_area.create_window(400, 0, anchor=N, window=button1)
-
     buttonred = Button(root, command = buttred, anchor = N)
     buttonred.configure(width = 3, background = "#FF0000", relief = FLAT)
-    buttonred_window = drawing_area.create_window(500, 0, anchor=N, window=buttonred)
+    buttonred_window = drawing_area.create_window(740, 0, anchor=N, window=buttonred)
 
     buttonblack = Button(root, command = buttblack, anchor = N)
     buttonblack.configure(width = 3, background = "#000000", relief = FLAT)
-    buttonblack_window = drawing_area.create_window(550, 0, anchor=N, window=buttonblack)
+    buttonblack_window = drawing_area.create_window(790, 0, anchor=N, window=buttonblack)
 
     buttongreen = Button(root, command = buttgreen, anchor = N)
     buttongreen.configure(width = 3, background = "#00FF00", relief = FLAT)
-    buttongreen_window = drawing_area.create_window(600, 0, anchor=N, window=buttongreen)
+    buttongreen_window = drawing_area.create_window(840, 0, anchor=N, window=buttongreen)
 
     buttonblue = Button(root, command = buttblue, anchor = N)
     buttonblue.configure(width = 3, background = "#0000FF", relief = FLAT)
-    buttonblue_window = drawing_area.create_window(650, 0, anchor=N, window=buttonblue)
+    buttonblue_window = drawing_area.create_window(890, 0, anchor=N, window=buttonblue)
 
     buttonyellow = Button(root, command = buttyellow, anchor = N)
     buttonyellow.configure(width = 3, background = "#FFFF00", relief = FLAT)
-    buttonyellow_window = drawing_area.create_window(700, 0, anchor=N, window=buttonyellow)
+    buttonyellow_window = drawing_area.create_window(940, 0, anchor=N, window=buttonyellow)
+
+    button1 = Button(root, text = "Clear", command = restart_program, anchor = N)
+    button1.configure(width = 3, background = "#FFFFFF", relief = FLAT)
+    button1_window = drawing_area.create_window(640, 0, anchor=N, window=button1)
+
+    buttoneraser = Button(root, text="Eraser", command = butter, anchor = N)
+    buttoneraser.configure(width = 3, background = "#FFFFFF", relief = FLAT)
+    buttoneraser_window = drawing_area.create_window(690, 0, anchor=N, window=buttoneraser)
+
+    buttonquote = Button(root, text="Null", command ="", anchor = N)
+    buttonquote.configure(width = 3, background = "#FFFFFF", relief = FLAT)
+    buttonquote_window = drawing_area.create_window(590, 0, anchor=N, window=buttonquote)
 
     buttongrey = Button(root, command = buttgrey, anchor = N)
     buttongrey.configure(width = 3, background = "#808080", relief = FLAT)
-    buttongrey_window = drawing_area.create_window(300, 0, anchor=N, window=buttongrey)    
+    buttongrey_window = drawing_area.create_window(540, 0, anchor=N, window=buttongrey)    
 
     buttonpurple = Button(root, command = buttpurple, anchor = N)
     buttonpurple.configure(width = 3, background = "#800080", relief = FLAT)
-    buttonpurple_window = drawing_area.create_window(250, 0, anchor=N, window=buttonpurple)
+    buttonpurple_window = drawing_area.create_window(490, 0, anchor=N, window=buttonpurple)
 
     buttonorange = Button(root, command = buttorange, anchor = N)
     buttonorange.configure(width = 3, background = "#FFA500", relief = FLAT)
-    buttonorange_window = drawing_area.create_window(200, 0, anchor=N, window=buttonorange)
+    buttonorange_window = drawing_area.create_window(440, 0, anchor=N, window=buttonorange)
 
     buttonbrown = Button(root, command = buttbrown, anchor = N)
     buttonbrown.configure(width = 3, background = "#A52A2A", relief = FLAT)
-    buttonbrown_window = drawing_area.create_window(150, 0, anchor=N, window=buttonbrown)
-
-    buttoncyan = Button(root, command = buttcyan, anchor = N)
-    buttoncyan.configure(width = 3, background = "#00FFFF", relief = FLAT)
-    buttoncyan_window = drawing_area.create_window(150, 0, anchor=N, window=buttoncyan)
-
-    buttonadd = Button(root, text="+", command = buttadd, anchor = N)
-    buttonadd.configure(width = 3, background = "#FFFFFF", relief = FLAT)
-    buttonadd_window = drawing_area.create_window(50, 0, anchor=N, window=buttonadd)
+    buttonbrown_window = drawing_area.create_window(390, 0, anchor=N, window=buttonbrown)
 
     buttonsub = Button(root, text="-", command = buttsub, anchor = N)
     buttonsub.configure(width = 3, background = "#FFFFFF", relief = FLAT)
-    buttonsub_window = drawing_area.create_window(100, 0, anchor=N, window=buttonsub)
+    buttonsub_window = drawing_area.create_window(290, 0, anchor=N, window=buttonsub)
 
-    root.geometry('800x600')
-    root.geometry('+400+200')
+    buttonadd = Button(root, text="+", command = buttadd, anchor = N)
+    buttonadd.configure(width = 3, background = "#FFFFFF", relief = FLAT)
+    buttonadd_window = drawing_area.create_window(240, 0, anchor=N, window=buttonadd)
+
+    root.geometry('1280x720')
+    root.geometry('+200+200')
     #root.overrideredirect(True)         #No border
     root.mainloop()
 
@@ -114,9 +118,13 @@ def buttbrown():
     global color
     color= "brown"
 
-def buttcyan():
+def butter():
     global color
-    color= "cyan"
+    color= "white"
+
+def buttquotes():
+    quotevar = os.popen("wget --timeout=3 -O - -q http://www.iheartquotes.com/api/v1/random").read()
+    drawing_area.insert(INSERT, quotevar)
 
 def buttadd():
     global linesize
